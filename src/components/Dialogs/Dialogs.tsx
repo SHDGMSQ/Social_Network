720551bdfcd7470c273215aa95d8e50f0ca8d5ef
Dmitry <sh.dg.msq@mail.ru>
2021-11-20 20:43:37 +0300

    after 22 video

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,12 +1,48 @@
 import React from "react";
 import s from './Dialogs.module.css'
-
+import {NavLink} from "react-router-dom";
 
 
 const Dialogs = (props:any) => {
     return (
         <div>
-            Dialogs + 1
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog}>
+                        <NavLink to="/dialogs/1">Dima</NavLink>
+                    </div>
+                    <div className={s.dialog}>
+                        <NavLink to="/dialogs/2">Andrey</NavLink>
+                    </div>
+                    <div className={s.dialog}>
+                        <NavLink to="/dialogs/3">Yuri</NavLink>
+                    </div>
+                    <div className={s.dialog}>
+                        <NavLink to="/dialogs/4">Sveta</NavLink>
+                    </div>
+                    <div className={s.dialog}>
+                        <NavLink to="/dialogs/5">John</NavLink>
+                    </div>
+                    <div className={s.dialog}>
+                        <NavLink to="/dialogs/6">Michail</NavLink>
+                    </div>
+                    <div className={s.dialog}>
+                        <NavLink to="/dialogs/7">Alexander</NavLink>
+                    </div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>
+                        Hi
+                    </div>
+                    <div className={s.message}>
+                        How are you?
+                    </div>
+                    <div className={s.message}>
+                        Yo
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
